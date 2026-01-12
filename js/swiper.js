1569734009b53f2743a20459a3db633f6f0f9666
Yosshiii43 <daisy440440@gmail.com>
2026-01-13 04:24:09 +0900

    change:swiperのエラー解消

--- js/swiper.js
+++ js/swiper.js
@@ -2,13 +2,11 @@
  * swiper
  ********************************************************************************/
 
-const slideLength = document.querySelectorAll('.swiper-slide').length;
-
 const mySwiper = new Swiper(".swiper", {
   slidesPerView: "auto", 
   spaceBetween: 24,
   loop: true, 
-  loopAdditionalSlides: 2, // 無限ループさせる場合に複製するスライド数
+  loopAdditionalSlides: 6, // 無限ループさせる場合に複製するスライド数
   speed: 8000, 
   allowTouchMove: false,
   autoplay: {
@@ -23,20 +21,20 @@ const mySwiper = new Swiper(".swiper", {
   },
 });
 
-
+/*
 // スライドを複製して追加
 const slides = document.querySelectorAll('.swiper-slide');
 
 for ( let i = 0; i <= 1; i++){// 2回繰り返す
-slides.forEach((slide) => {
-  const clone = slide.cloneNode(true);
-  //複製分のaltを削除
-  const cloneImage = clone.querySelector('img');
-  cloneImage.removeAttribute('alt');
-  // 複製したスライドをSwiperのコンテナに追加
-  mySwiper.appendSlide(clone);
-});
+  slides.forEach((slide) => {
+    const clone = slide.cloneNode(true);
+    //複製分のaltを削除
+    const cloneImage = clone.querySelector('img');
+    cloneImage.removeAttribute('alt');
+    // 複製したスライドをSwiperのコンテナに追加
+    mySwiper.appendSlide(clone);
+  });
 }
-
-// Swiperを更新して新しいスライドを認識させる（通常は自動的に更新されますが、念のため）
-mySwiper.update();
+*/
+// Swiperを更新して新しいスライドを認識させる
+//mySwiper.update();
